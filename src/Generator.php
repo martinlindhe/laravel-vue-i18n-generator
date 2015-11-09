16d840d08a27b5ee9ddc4fd3900dc58ed8d6f815
Martin Lindhe <martin@ubique.se>
2015-11-09 11:08:24 +0100

    add a test for basic functionality

---
+++ src/Generator.php
@@ -0,0 +1,67 @@
+<?php namespace MartinLindhe\VueInternationalizationGenerator;
+
+use DirectoryIterator;
+use Exception;
+
+class Generator
+{
+    /**
+     * @param string $path
+     * @return string
+     * @throws Exception
+     */
+    public function generateFromPath($path)
+    {
+        if (!is_dir($path)) {
+            throw new Exception('Directory not found: '.$path);
+        }
+
+        $locales = [];
+        $dir = new DirectoryIterator($path);
+        foreach ($dir as $fileinfo) {
+            if (!$fileinfo->isDot()
+                && $fileinfo->isDir()
+                && !in_array($fileinfo->getFilename(), ['vendor'])
+            ) {
+                $locales[$fileinfo->getFilename()] =
+                    $this->allocateLocaleArray($path . '/' . $fileinfo->getFilename());
+            }
+        }
+
+        return 'export default '
+            . json_encode($locales, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) . PHP_EOL;
+    }
+
+    /**
+     * @param string $path
+     * @return array
+     */
+    private function allocateLocaleArray($path)
+    {
+        $data = [];
+
+        $dir = new DirectoryIterator($path);
+        foreach ($dir as $fileinfo) {
+            if (!$fileinfo->isDot()) {
+                $noExt = $this->removeExtension($fileinfo->getFilename());
+                $data[$noExt] = include($path . '/' . $fileinfo->getFilename());
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param string $filename
+     * @return string
+     */
+    private function removeExtension($filename)
+    {
+        $pos = mb_strrpos($filename, '.');
+        if ($pos === false) {
+            return $filename;
+        }
+
+        return mb_substr($filename, 0, $pos);
+    }
+}
---
+++ tests/GenerateTest.php
@@ -0,0 +1,90 @@
+<?php
+
+use MartinLindhe\VueInternationalizationGenerator\Generator;
+
+class GenerateTest extends \PHPUnit_Framework_TestCase
+{
+    private function generateLocaleFilesFrom(array $arr)
+    {
+        $root = sys_get_temp_dir() . '/' . sha1(microtime(true) . mt_rand());
+
+        if (!is_dir($root)) {
+            mkdir($root, 0777, true);
+        }
+
+        foreach ($arr as $key => $val) {
+
+            if (!is_dir($root . '/' . $key)) {
+                mkdir($root . '/' . $key);
+            }
+
+            foreach ($val as $group => $content) {
+                $outFile = $root . '/'. $key . '/' . $group . '.php';
+                file_put_contents($outFile, '<?php return ' . var_export($content, true) . ';');
+            }
+        }
+
+        return $root;
+    }
+
+    private function destroyLocaleFilesFrom(array $arr, $root)
+    {
+        foreach ($arr as $key => $val) {
+
+            foreach ($val as $group => $content) {
+                $outFile = $root . '/'. $key . '/' . $group . '.php';
+                if (file_exists($outFile)) {
+                    unlink($outFile);
+                }
+            }
+
+            if (is_dir($root . '/' . $key)) {
+                rmdir($root . '/' . $key);
+            }
+
+        }
+
+        if (is_dir($root)) {
+            rmdir($root);
+        }
+    }
+
+    function testBasic()
+    {
+        $arr = [
+            'en' => [
+                'help' => [
+                    'yes' => 'yes',
+                    'no' => 'no',
+                ]
+            ],
+            'sv' => [
+                'help' => [
+                    'yes' => 'ja',
+                    'no' => 'nej',
+                ]
+            ]
+        ];
+
+        $root = $this->generateLocaleFilesFrom($arr);
+
+        $this->assertEquals(
+            'export default {' . PHP_EOL
+            . '    "en": {' . PHP_EOL
+            . '        "help": {' . PHP_EOL
+            . '            "yes": "yes",' . PHP_EOL
+            . '            "no": "no"' . PHP_EOL
+            . '        }' . PHP_EOL
+            . '    },' . PHP_EOL
+            . '    "sv": {' . PHP_EOL
+            . '        "help": {' . PHP_EOL
+            . '            "yes": "ja",' . PHP_EOL
+            . '            "no": "nej"' . PHP_EOL
+            . '        }' . PHP_EOL
+            . '    }' . PHP_EOL
+            . '}' . PHP_EOL,
+            (new Generator)->generateFromPath($root));
+
+        $this->destroyLocaleFilesFrom($arr, $root);
+    }
+}
