martinlindhe/laravel-vue-i18n-generator

commit 53426829a8fe52e94d2bb9c5bc83776ed9317a81

rework internals, make tests more robust

src/GeneratorProvider.php
@@ -18,10 +18,6 @@ class GeneratorProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->app->singleton('vue-i18n.generate', function () {
-            return new Commands\GenerateInclude;
-        });
-
         $this->commands(
             'vue-i18n.generate'
         );
@@ -43,6 +39,9 @@ class GeneratorProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->singleton(Generator::class, function ($app) {
+            return new Generator($app['config']['vue-i18n-generator']);
+        });
     }
 
     /**

src/Commands/GenerateInclude.php
@@ -27,26 +27,22 @@ class GenerateInclude extends Command
     public function handle()
     {
         $root = base_path() . config('vue-i18n-generator.langPath');
+        $config = config('vue-i18n-generator');
 
+        // options
         $umd = $this->option('umd');
-
         $multipleFiles = $this->option('multi');
-
         $withVendor = $this->option('with-vendor');
 
-        $i18nLib = config('vue-i18n-generator.i18nLib');
-
         if ($multipleFiles) {
-            $files = (new Generator($i18nLib))
+            $files = (new Generator($config))
                 ->generateMultiple($root, $umd);
             echo "Written to :" . PHP_EOL . $files . PHP_EOL;
             exit();
         }
 
-        $data = (new Generator($i18nLib))
+        $data = (new Generator($config))
             ->generateFromPath($root, $umd, $withVendor);
-
-
 
         $jsFile = base_path() . config('vue-i18n-generator.jsFile');
         file_put_contents($jsFile, $data);

src/Generator.php
@@ -6,6 +6,7 @@ use App;
 
 class Generator
 {
+    private $config;
 
     private $availableLocales = [];
     private $filesToCreate = [];
@@ -13,16 +14,17 @@ class Generator
     const VUEX_I18N = 'vuex-i18n';
     const VUE_I18N = 'vue-i18n';
 
-    private $i18nLib;
-
     /**
      * The constructor
      *
-     * @param string $i18nLib
+     * @param array $config
      */
-    public function __construct($i18nLib = self::VUE_I18N)
+    public function __construct($config = [])
     {
-        $this->i18nLib = $i18nLib;
+        if (!isset($config['i18nLib'])) {
+            $config['i18nLib'] = self::VUE_I18N;
+        }
+        $this->config = $config;
     }
 
     /**
@@ -39,6 +41,7 @@ class Generator
         }
 
         $locales = [];
+        $files = [];
         $dir = new DirectoryIterator($path);
         $jsBody = '';
         foreach ($dir as $fileinfo) {
@@ -47,20 +50,27 @@ class Generator
                     continue;
                 }
 
-                $noExt = $this->removeExtension($fileinfo->getFilename());
+                $files[] = $fileinfo->getRealPath();
+            }
+        }
+        asort($files);
 
-                if ($fileinfo->isDir()) {
-                    $local = $this->allocateLocaleArray($fileinfo->getRealPath());
-                } else {
-                    $local = $this->allocateLocaleJSON($fileinfo->getRealPath());
-                    if ($local === null) continue;
-                }
+        foreach ($files as $fileName) {
+            $fileinfo = new \SplFileInfo($fileName);
 
-                if (isset($locales[$noExt])) {
-                    $locales[$noExt] = array_merge($local, $locales[$noExt]);
-                } else {
-                    $locales[$noExt] = $local;
-                }
+            $noExt = $this->removeExtension($fileinfo->getFilename());
+
+            if ($fileinfo->isDir()) {
+                $local = $this->allocateLocaleArray($fileinfo->getRealPath());
+            } else {
+                $local = $this->allocateLocaleJSON($fileinfo->getRealPath());
+                if ($local === null) continue;
+            }
+
+            if (isset($locales[$noExt])) {
+                $locales[$noExt] = array_merge($local, $locales[$noExt]);
+            } else {
+                $locales[$noExt] = $local;
             }
         }
 
@@ -92,7 +102,7 @@ class Generator
         if (!is_dir($path)) {
             throw new Exception('Directory not found: ' . $path);
         }
-        $jsPath = base_path() . config('vue-i18n-generator.jsPath');
+        $jsPath = base_path() . $this->config['jsPath'];
         $locales = [];
         $fileToCreate = '';
         $createdFiles = '';
@@ -183,8 +193,7 @@ class Generator
             if ($fileinfo->isDir()) {
                 // Recursivley iterate through subdirs, until everything is allocated.
 
-                $data[$fileinfo->getFilename()] =
-                    $this->allocateLocaleArray($path . '/' . $fileinfo->getFilename());
+                $data[$fileinfo->getFilename()] = $this->allocateLocaleArray($path . '/' . $fileinfo->getFilename());
             } else {
                 $noExt = $this->removeExtension($fileinfo->getFilename());
                 $fileName = $path . '/' . $fileinfo->getFilename();
@@ -194,8 +203,7 @@ class Generator
                     continue;
                 }
 
-                $langFiles = config('vue-i18n-generator.langFiles');
-                if(! is_null($langFiles) && ! empty($langFiles) && ! in_array($noExt, $langFiles)) {
+                if (!empty($langFiles) && !in_array($noExt, $langFiles)) {
                     continue;
                 }
 
@@ -205,8 +213,8 @@ class Generator
                     throw new Exception('Unexpected data while processing ' . $fileName);
                     continue;
                 }
-                if($lastLocale !== false){
-                    $root = realpath(base_path() . config('vue-i18n-generator.langPath') . '/' . $lastLocale);
+                if ($lastLocale !== false) {
+                    $root = realpath(base_path() . $this->config['langPath'] . '/' . $lastLocale);
                     $filePath = $this->removeExtension(str_replace('\\', '_', ltrim(str_replace($root, '', realpath($fileName)), '\\')));
                     $this->filesToCreate[$filePath][$lastLocale] = $this->adjustArray($tmp);
                 }
@@ -261,8 +269,7 @@ class Generator
     }
 
     /**
-     * Turn Laravel style ":link" into vue-i18n style "{link}" and
-     * turn Laravel style "|" into vuex-i18n style ":::" when using vuex-i18n.
+     * Turn Laravel style ":link" into vue-i18n style "{link}" or vuex-i18n style ":::".
      *
      * @param string $s
      * @return string
@@ -273,10 +280,9 @@ class Generator
             return $s;
         }
 
-        if ($this->i18nLib === self::VUEX_I18N) {
+        if ($this->config['i18nLib'] === self::VUEX_I18N) {
             $searchPipePattern = '/(\s)*(\|)(\s)*/';
             $threeColons = ' ::: ';
-
             $s = preg_replace($searchPipePattern, $threeColons, $s);
         }
 

src/config/vue-i18n-generator.php
@@ -49,8 +49,7 @@ return [
     |
     | Specify the library you use for localization.
     | Options are vue-i18n or vuex-i18n.
-    | You can also use Generator::VUE_I18N or Generator::VUEX_I18N
     |
     */
-    'i18nLib' => \MartinLindhe\VueInternationalizationGenerator\Generator::VUE_I18N,
+    'i18nLib' => 'vue-i18n',
 ];

tests/GenerateTest.php
@@ -27,27 +27,6 @@ class GenerateTest extends \PHPUnit_Framework_TestCase
         return $root;
     }
 
-    private function generateLocaleFilesFromJSON(array $arr)
-    {
-        $root = sys_get_temp_dir() . '/' . sha1(microtime(true) . mt_rand());
-
-        if (!is_dir($root)) {
-            mkdir($root, 0777, true);
-        }
-
-        foreach ($arr as $key => $val) {
-
-            if (!is_dir($root . '/' . $key)) {
-                mkdir($root . '/' . $key);
-            }
-
-            $outFile = $root . '/'. $key . '.json';
-            file_put_contents($outFile, json_encode($val));
-        }
-
-        return $root;
-    }
-
     private function destroyLocaleFilesFrom(array $arr, $root)
     {
         foreach ($arr as $key => $val) {
@@ -63,27 +42,6 @@ class GenerateTest extends \PHPUnit_Framework_TestCase
                 rmdir($root . '/' . $key);
             }
 
-        }
-
-        if (is_dir($root)) {
-            rmdir($root);
-        }
-    }
-
-    private function destroyLocaleFilesFromJSON(array $arr, $root)
-    {
-        foreach ($arr as $key => $val) {
-
-            foreach ($val as $group => $content) {
-                $outFile = $root . '/'. $key . '.json';
-                if (file_exists($outFile)) {
-                    unlink($outFile);
-                }
-            }
-
-            if (is_dir($root . '/' . $key)) {
-                rmdir($root . '/' . $key);
-            }
         }
 
         if (is_dir($root)) {
@@ -107,6 +65,7 @@ class GenerateTest extends \PHPUnit_Framework_TestCase
                 ]
             ]
         ];
+
         $root = $this->generateLocaleFilesFrom($arr);
         $this->assertEquals(
             'export default {' . PHP_EOL
@@ -123,7 +82,7 @@ class GenerateTest extends \PHPUnit_Framework_TestCase
             . '        }' . PHP_EOL
             . '    }' . PHP_EOL
             . '}' . PHP_EOL,
-            (new Generator)->generateFromPath($root));
+            (new Generator([]))->generateFromPath($root));
         $this->destroyLocaleFilesFrom($arr, $root);
     }
 
@@ -189,7 +148,7 @@ class GenerateTest extends \PHPUnit_Framework_TestCase
             . '        }' . PHP_EOL
             . '    }' . PHP_EOL
             . '}' . PHP_EOL,
-            (new Generator)->generateFromPath($root, false, true));
+            (new Generator([]))->generateFromPath($root, false, true));
 
         $this->destroyLocaleFilesFrom($arr, $root);
     }
@@ -228,7 +187,7 @@ class GenerateTest extends \PHPUnit_Framework_TestCase
             . '        }' . PHP_EOL
             . '    }' . PHP_EOL
             . '}' . PHP_EOL,
-            (new Generator('vuex-i18n'))->generateFromPath($root));
+            (new Generator([]))->generateFromPath($root));
 
         $this->destroyLocaleFilesFrom($arr, $root);
     }
@@ -259,26 +218,9 @@ class GenerateTest extends \PHPUnit_Framework_TestCase
             . '        }' . PHP_EOL
             . '    }' . PHP_EOL
             . '}' . PHP_EOL,
-            (new Generator)->generateFromPath($root));
+            (new Generator([]))->generateFromPath($root));
 
         $this->destroyLocaleFilesFrom($arr, $root);
-
-        $root = $this->generateLocaleFilesFromJSON($arr);
-
-        $this->assertEquals(
-            'export default {' . PHP_EOL
-            . '    "en": {' . PHP_EOL
-            . '        "help": {' . PHP_EOL
-            . '            "yes": "see {link} y {lonk}",' . PHP_EOL
-            . '            "no": {' . PHP_EOL
-            . '                "one": "see {link}"' . PHP_EOL
-            . '            }' . PHP_EOL
-            . '        }' . PHP_EOL
-            . '    }' . PHP_EOL
-            . '}' . PHP_EOL,
-            (new Generator)->generateFromPath($root));
-
-        $this->destroyLocaleFilesFromJSON($arr, $root);
     }
 
     function testShouldNotTouchHtmlTags()
@@ -303,38 +245,19 @@ class GenerateTest extends \PHPUnit_Framework_TestCase
             . '        }' . PHP_EOL
             . '    }' . PHP_EOL
             . '}' . PHP_EOL,
-            (new Generator)->generateFromPath($root));
+            (new Generator([]))->generateFromPath($root));
 
         $this->destroyLocaleFilesFrom($arr, $root);
-
-        $root = $this->generateLocaleFilesFromJSON($arr);
-
-        $this->assertEquals(
-            'export default {' . PHP_EOL
-            . '    "en": {' . PHP_EOL
-            . '        "help": {' . PHP_EOL
-            . '            "yes": "see <a href=\"mailto:mail@com\">",' . PHP_EOL
-            . '            "no": "see <a href=\"{link}\">"' . PHP_EOL
-            . '        }' . PHP_EOL
-            . '    }' . PHP_EOL
-            . '}' . PHP_EOL,
-            (new Generator)->generateFromPath($root));
-
-        $this->destroyLocaleFilesFromJSON($arr, $root);
     }
 
-    function testPlurals()
+    function testPluralization()
     {
         $arr = [
             'en' => [
                 'plural' => [
                     'one' => 'There is one apple|There are many apples',
                     'two' => 'There is one apple | There are many apples',
-                    'three' => 'There is one apple    | There are many apples',
-                    'four' => 'There is one apple |     There are many apples',
                     'five' => [
-                        'one' => 'There is one apple|There are many apples',
-                        'two' => 'There is one apple | There are many apples',
                         'three' => 'There is one apple    | There are many apples',
                         'four' => 'There is one apple |     There are many apples',
                     ]
@@ -344,49 +267,38 @@ class GenerateTest extends \PHPUnit_Framework_TestCase
 
         $root = $this->generateLocaleFilesFrom($arr);
 
+        // vue-i18n
+        $this->assertEquals(
+            'export default {' . PHP_EOL
+            . '    "en": {' . PHP_EOL
+            . '        "plural": {' . PHP_EOL
+            . '            "one": "There is one apple|There are many apples",' . PHP_EOL
+            . '            "two": "There is one apple | There are many apples",' . PHP_EOL
+            . '            "five": {' . PHP_EOL
+            . '                "three": "There is one apple    | There are many apples",' . PHP_EOL
+            . '                "four": "There is one apple |     There are many apples"' . PHP_EOL
+            . '            }' . PHP_EOL
+            . '        }' . PHP_EOL
+            . '    }' . PHP_EOL
+            . '}' . PHP_EOL,
+            (new Generator(['i18nLib' => 'vue-i18n']))->generateFromPath($root));
+
+        // vuex-i18n
         $this->assertEquals(
             'export default {' . PHP_EOL
             . '    "en": {' . PHP_EOL
             . '        "plural": {' . PHP_EOL
             . '            "one": "There is one apple ::: There are many apples",' . PHP_EOL
             . '            "two": "There is one apple ::: There are many apples",' . PHP_EOL
-            . '            "three": "There is one apple ::: There are many apples",' . PHP_EOL
-            . '            "four": "There is one apple ::: There are many apples",' . PHP_EOL
             . '            "five": {' . PHP_EOL
-            . '                "one": "There is one apple ::: There are many apples",' . PHP_EOL
-            . '                "two": "There is one apple ::: There are many apples",' . PHP_EOL
             . '                "three": "There is one apple ::: There are many apples",' . PHP_EOL
             . '                "four": "There is one apple ::: There are many apples"' . PHP_EOL
             . '            }' . PHP_EOL
             . '        }' . PHP_EOL
             . '    }' . PHP_EOL
             . '}' . PHP_EOL,
-            (new Generator('vuex-i18n'))->generateFromPath($root));
+            (new Generator(['i18nLib' => 'vuex-i18n']))->generateFromPath($root));
 
         $this->destroyLocaleFilesFrom($arr, $root);
-
-        $root = $this->generateLocaleFilesFromJSON($arr);
-
-        $this->assertEquals(
-            'export default {' . PHP_EOL
-            . '    "en": {' . PHP_EOL
-            . '        "plural": {' . PHP_EOL
-            . '            "one": "There is one apple ::: There are many apples",' . PHP_EOL
-            . '            "two": "There is one apple ::: There are many apples",' . PHP_EOL
-            . '            "three": "There is one apple ::: There are many apples",' . PHP_EOL
-            . '            "four": "There is one apple ::: There are many apples",' . PHP_EOL
-            . '            "five": {' . PHP_EOL
-            . '                "one": "There is one apple ::: There are many apples",' . PHP_EOL
-            . '                "two": "There is one apple ::: There are many apples",' . PHP_EOL
-            . '                "three": "There is one apple ::: There are many apples",' . PHP_EOL
-            . '                "four": "There is one apple ::: There are many apples"' . PHP_EOL
-            . '            }' . PHP_EOL
-            . '        }' . PHP_EOL
-            . '    }' . PHP_EOL
-            . '}' . PHP_EOL,
-            (new Generator('vuex-i18n'))->generateFromPath($root));
-
-        $this->destroyLocaleFilesFromJSON($arr, $root);
     }
-
 }

commit 49bc4c9422721ccc07249474412d4b9881d93e15

fix issue introduced in 53426829a8fe52e94d2bb9c5bc83776ed9317a81, fixes #48

src/GeneratorProvider.php
@@ -39,7 +39,7 @@ class GeneratorProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(Generator::class, function ($app) {
+        $this->app->singleton('vue-i18n.generate', function ($app) {
             return new Generator($app['config']['vue-i18n-generator']);
         });
     }