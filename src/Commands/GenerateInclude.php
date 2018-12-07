55c2d33c0bd3c681f38dc683b6f4ac600b1da557
Jan <jan.te@icloud.com>
2018-12-07 17:53:14 +0100

    Add file name and lang files command line options (#70)
    * Add command line option for output file name
    
    * Add command line option for lang files
    
    * Make tests pass

--- src/Commands/GenerateInclude.php
+++ src/Commands/GenerateInclude.php
@@ -11,7 +11,7 @@ class GenerateInclude extends Command
      *
      * @var string
      */
-    protected $signature = 'vue-i18n:generate {--umd} {--multi} {--with-vendor}';
+    protected $signature = 'vue-i18n:generate {--umd} {--multi} {--with-vendor} {--file-name=} {--lang-files=}';
 
     /**
      * The console command description.
@@ -33,6 +33,8 @@ class GenerateInclude extends Command
         $umd = $this->option('umd');
         $multipleFiles = $this->option('multi');
         $withVendor = $this->option('with-vendor');
+        $fileName = $this->option('file-name');
+        $langFiles = $this->option('lang-files');
 
         if ($multipleFiles) {
             $files = (new Generator($config))
@@ -45,14 +47,32 @@ class GenerateInclude extends Command
             return;
         }
 
-        $data = (new Generator($config))
-            ->generateFromPath($root, $umd, $withVendor);
+        if ($langFiles) {
+            $langFiles = explode(',', $langFiles);
+        }
 
-        $jsFile = base_path() . config('vue-i18n-generator.jsFile');
+        $data = (new Generator($config))
+            ->generateFromPath($root, $umd, $withVendor, $langFiles);
+
+
+        $jsFile = $this->getFileName($fileName);
         file_put_contents($jsFile, $data);
 
         if ($config['showOutputMessages']) {
             $this->info("Written to : ".$jsFile);
         }
+    }
+
+    /**
+     * @param string $fileNameOption
+     * @return string
+     */
+    private function getFileName($fileNameOption)
+    {
+        if (isset($fileNameOption)) {
+            return base_path() . $fileNameOption;
+        }
+
+         return base_path() . config('vue-i18n-generator.jsFile');
     }
 }
