8e51fed819c00851ed28df4e95799cb5cf9760e5
Johnny Charcosset <36884315+jcharcosset@users.noreply.github.com>
2019-06-26 12:38:47 +0200

    Remove assets from path folder in config file (#88)
    For new Laravel 5.7 applications, the assets directory that contains the scripts and styles has been flattened into the resources directory. 
    
    @see https://laravel.com/docs/5.7/upgrade

--- src/config/vue-i18n-generator.php
+++ src/config/vue-i18n-generator.php
@@ -55,8 +55,8 @@ return [
     | Note: the path will be prepended to point to the App directory.
     |
     */
-    'jsPath' => '/resources/assets/js/langs/',
-    'jsFile' => '/resources/assets/js/vue-i18n-locales.generated.js',
+    'jsPath' => '/resources/js/langs/',
+    'jsFile' => '/resources/js/vue-i18n-locales.generated.js',
 
     /*
     |--------------------------------------------------------------------------
